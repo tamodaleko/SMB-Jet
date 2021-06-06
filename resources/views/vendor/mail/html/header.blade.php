0fa16311fa17636e0a544953eebf985730c9f3ce
Dejan Prole <shimids89@gmail.com>
2021-06-07 01:37:27 +0200

    Change email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('img/logo.png') }}" class="logo" alt="SMB Jet Logo" style="width: 200px; height: auto;">
+</a>
+</td>
+</tr>
